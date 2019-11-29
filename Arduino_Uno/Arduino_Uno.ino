e74c2ebd995562b115426f511dcae6521ad745fa
Sulaiman <sulaiman_666@apps.ipb.ac.id>
2019-11-29 13:43:17 +0700

    Arduino_uno v2
    Tanggal + waktu belum sempurna

--- Arduino_Uno/Arduino_Uno.ino
+++ Arduino_Uno/Arduino_Uno.ino
@@ -7,7 +7,9 @@
 //Inisiasi RTC
 DS3231 rtc(SDA,SCL);
 Time waktu;
-unsigned long jeda = 0;
+
+//Trigger
+unsigned long id = 0;
 
 //Pemetaan pin sensor
 #define sensor0 A0
@@ -16,8 +18,10 @@ unsigned long jeda = 0;
 #define sensor3 A3
 
 //Pemetaan pin Relay
-#define relay0 D0
-#define relay1 D1
+#define relay0 3
+#define relay1 4
+#define relay2 7
+#define relay3 8
 
 //Inisiasi layar LCD
 LiquidCrystal_I2C lcd(0x27, 20, 4);
@@ -27,8 +31,8 @@ SoftwareSerial s(5,6);
 
 void setup() {
   // put your setup code here, to run once:
-  s.begin(115200);
-  Serial.begin(115200);
+  s.begin(9600);
+  Serial.begin(9600);
 
   //Penentuan mode pin sensor
   pinMode(sensor0, INPUT);
@@ -39,6 +43,8 @@ void setup() {
   //Pemetaan mode pinRelay
   pinMode(relay0, OUTPUT);
   pinMode(relay1, OUTPUT);
+  pinMode(relay2, OUTPUT);
+  pinMode(relay3, OUTPUT);
 
   //Begin LCD
   lcd.begin();
@@ -47,43 +53,37 @@ void setup() {
   lcd.backlight();
   lcd.setCursor(0,0);
   lcd.print(" Automisasi Tanaman");
-  lcd.setCursor(1,0);
+  lcd.setCursor(0,1);
   lcd.print("       Cabai");
-  lcd.setCursor(2,0);
+  lcd.setCursor(0,2);
   lcd.print("    Version Beta");
-  lcd.setCursor(3,0);
+  lcd.setCursor(0,3);
   lcd.print("    Initializing");
 
   //Inisiasi Jam
   rtc.begin();
-
-  //Konfigurasi jam
-  rtc.setDOW(SELASA);       //Set hari
-  rtc.setTime(00, 47, 00);  //Set jam
-  rtc.setDate(27, 11, 2019);//Set Tanggal
-
-  jeda = millis();
-  while(millis() < jeda + 5000){
-    //Waiting
-  }
+  //rtc.setDate(29, 11, 2019);     // Set tanggal 20 September 2016
+  
+  delay(5000);
 }
 
 void loop() {
+  //jeda = millis();
+
+  String pesan="";
+  
   //LCD Greet
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print(" Automisasi Tanaman");
-  lcd.setCursor(1,0);
+  lcd.setCursor(0,1);
   lcd.print("       Cabai");
-  lcd.setCursor(2,0);
+  lcd.setCursor(0,2);
   lcd.print("Memeriksa Kelembapan");
-  lcd.setCursor(3,0);
+  lcd.setCursor(0,3);
   lcd.print("      Scanning");
 
-  jeda = millis();
-  while(millis() < jeda + 5000){
-    //Waiting
-  }
+  delay(5000);
   
   //Menghitung kelembapan tanah
   int kelembapan0 = map(analogRead(sensor0),0,1023,100,0);//Mendapatkan value pada sensor0
@@ -93,82 +93,335 @@ void loop() {
 
   //Mengelompokkan berdasarkan sinar matahari
   int kelompok0 = (kelembapan0+kelembapan1)/2;
+  Serial.println(kelompok0);
   int kelompok1 = (kelembapan2+kelembapan3)/2;
+  Serial.println(kelompok1);
 
   //Conditioning berdasarkan kelompok
-  if(kelompok0<60){
+  if(kelompok1<60 && kelompok0<60){
+    //Update Trigger
+    id++;
+    Serial.println(id+" 3");
+    
     //Print LCD
     lcd.clear();
     lcd.setCursor(0,0);
     lcd.print(" Automisasi Tanaman");
-    lcd.setCursor(1,0);
-    lcd.print("       Cabai");
-    lcd.setCursor(2,0);
+    lcd.setCursor(0,1);
+    lcd.print("      Cabai");
+    lcd.setCursor(0,2);
     lcd.print("     Showering");
-    lcd.setCursor(3,0);
-    lcd.print("    Kelompok 0");
-    
-    //Menyalakan keran air
-    digitalWrite(relay0, HIGH);
-
+    lcd.setCursor(0,3);
+    lcd.print("  Kelompok 1 dan 2");
+  
     //Store kapan trigger terjadi
     waktu=rtc.getTime();
-    int jam = waktu.hour;
-    int menit = waktu.min;
-    int detik = waktu.sec
 
-    String tanggal = rtc.getDOWStr(FORMAT_SHORT);
+    //Store waktu
+    uint8_t jam = waktu.hour;
+    uint8_t menit = waktu.min;
+    uint8_t detik = waktu.sec;
+    
+    //Store tanggal
+    uint8_t hari = waktu.date;
+    uint8_t bulan = waktu.mon;
+    uint8_t tahun = waktu.year;
 
-    //Upload ke NodeMcu
+    //Menyalakan keran air
+    digitalWrite(relay0, HIGH);
+    digitalWrite(relay1, HIGH);
+    digitalWrite(relay2, HIGH);
+    digitalWrite(relay3, HIGH);
 
-    //Delay 30 detik
-    while(millis() < jeda + 30000){
-        //wait
+    //Transfer trigger
+    String index = String(id);
+    
+    //Transfer Sensor
+    String sensor = "|" ;
+    sensor += sensor0 ;
+    sensor += "|" ;
+    sensor += sensor1 ;
+    sensor += "|" ;
+    sensor += sensor2 ;
+    sensor +="|" ;
+    sensor+= sensor3;
+    
+    //Transfer Sensor Kelompok
+    String kelompok = "|" ;
+    kelompok += kelompok0 ;
+    kelompok += "|" ;
+    kelompok += kelompok1;
+    
+    //Transfer Waktu
+    String waktu = "|" ;
+    waktu += jam ;
+    waktu += "|" ;
+    waktu += menit; 
+    waktu += "|" ;
+    waktu += detik;
+
+    //Transfer tanggal
+    String tanggal = "|" ;
+    tanggal += hari ;
+    tanggal += "|" ;
+    tanggal += bulan ;
+    tanggal += "|" ;
+    tanggal += tahun;
+    tanggal += "|" ;
+
+    //Keseluruhan
+    pesan = index + sensor + kelompok + waktu + tanggal;
+    Serial.println(pesan);
+
+    for(int i = 0; i< pesan.length(); i++){
+      s.write(pesan[i]);  
     }
+
+//    if(s.available()>0){
+//      s.write(pesan);
+//    }
+
+    sensor = "";
+    kelompok = "";
+    waktu = "";
+    tanggal = "";
+    index = "";
+    
+    delay(30000);
 
     //Mematikankeran air
     digitalWrite(relay0, LOW);
+    digitalWrite(relay1, LOW);
+    digitalWrite(relay2, LOW);
+    digitalWrite(relay3, LOW);
 
     //Delay 30 menit untuk penyesuaian kelembapan
-    while(millis() < jeda + 1800000){
-        //wait
-    }  
-  }else if(kelompok1<60){
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(" Automisasi Tanaman");
+    lcd.setCursor(0,1);
+    lcd.print("       Cabai");
+    lcd.setCursor(0,2);
+    lcd.print("Memeriksa Kelembapan");
+    lcd.setCursor(0,3);
+    lcd.print("     Calibrating");
+    
+    delay(1800000);
+    
+  }
+  else if(kelompok0<60){
+    //Upload Trigger
+    id++;
+    Serial.println(id);
+    
     //Print LCD
     lcd.clear();
     lcd.setCursor(0,0);
     lcd.print(" Automisasi Tanaman");
-    lcd.setCursor(1,0);
+    lcd.setCursor(0,1);
     lcd.print("       Cabai");
-    lcd.setCursor(2,0);
+    lcd.setCursor(0,2);
     lcd.print("     Showering");
-    lcd.setCursor(3,0);
-    lcd.print("    Kelompok 1");
-    
-    //Menyalakan keran air
-    digitalWrite(relay1, HIGH);
+    lcd.setCursor(0,3);
+    lcd.print("    Kelompok 0");
 
     //Store kapan trigger terjadi
     waktu=rtc.getTime();
-    int jam = waktu.hour;
-    int menit = waktu.min;
-    int detik = waktu.sec
 
-    String tanggal = rtc.getDOWStr(FORMAT_SHORT);
+    //Store waktu
+    uint8_t jam = waktu.hour;
+    uint8_t menit = waktu.min;
+    uint8_t detik = waktu.sec;
+    
+    //Store tanggal
+    uint8_t hari = waktu.date;
+    uint8_t bulan = waktu.mon;
+    uint8_t tahun = waktu.year;
 
-    //Upload ke NodeMcu
+    //Menyalakan keran air
+    digitalWrite(relay0, HIGH);
+    digitalWrite(relay1, HIGH);
 
-    //Delay 30 detik
-    while(millis() < jeda + 30000){
-        //wait
+   //Transfer trigger
+    String index = String(id);
+    
+    //Transfer Sensor
+    String sensor = "|" ;
+    sensor += sensor0 ;
+    sensor += "|" ;
+    sensor += sensor1 ;
+    sensor += "|" ;
+    sensor += sensor2 ;
+    sensor +="|" ;
+    sensor+= sensor3;
+    
+    //Transfer Sensor Kelompok
+    String kelompok = "|" ;
+    kelompok += kelompok0 ;
+    kelompok += "|" ;
+    kelompok += kelompok1;
+    
+    //Transfer Waktu
+    String waktu = "|" ;
+    waktu += jam ;
+    waktu += "|" ;
+    waktu += menit; 
+    waktu += "|";
+    waktu += detik;
+
+    //Transfer tanggal
+    String tanggal = "|" ;
+    tanggal += hari ;
+    tanggal += "|" ;
+    tanggal += bulan ;
+    tanggal += "|" ;
+    tanggal += tahun;
+    tanggal += "|" ;
+
+    //Keseluruhan
+    pesan = index + sensor + kelompok + waktu + tanggal;
+    Serial.println(pesan);
+
+    for(int i = 0; i< pesan.length(); i++){
+      s.write(pesan[i]);  
     }
+
+//    if(s.available()>0){
+//      s.write(pesan);
+//    }
+
+    sensor = "";
+    kelompok = "";
+    waktu = "";
+    tanggal = "";
+    index = "";
+
+    delay(30000);
 
     //Mematikankeran air
+    digitalWrite(relay0, LOW);
     digitalWrite(relay1, LOW);
 
-    //Delay 30 menit untuk penyesuaian kelembapan
-    while(millis() < jeda + 1800000){
-        //wait
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(" Automisasi Tanaman");
+    lcd.setCursor(0,1);
+    lcd.print("       Cabai");
+    lcd.setCursor(0,2);
+    lcd.print("Memeriksa Kelembapan");
+    lcd.setCursor(0,3);
+    lcd.print("     Calibrating");
+
+     delay(1800000);
+  
+  }else if(kelompok1<60){
+    //Update Trigger
+    id++;
+    Serial.println(id+" 2");
+    
+    //Print LCD
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(" Automisasi Tanaman");
+    lcd.setCursor(0,1);
+    lcd.print("      Cabai");
+    lcd.setCursor(0,2);
+    lcd.print("     Showering");
+    lcd.setCursor(0,3);
+    lcd.print("    Kelompok 1");
+  
+    //Store kapan trigger terjadi
+    waktu=rtc.getTime();
+
+    //Store waktu
+    uint8_t jam = waktu.hour;
+    uint8_t menit = waktu.min;
+    uint8_t detik = waktu.sec;
+    
+    //Store tanggal
+    uint8_t hari = waktu.date;
+    uint8_t bulan = waktu.mon;
+    uint8_t tahun = waktu.year;
+
+    //Menyalakan keran air
+    digitalWrite(relay2, HIGH);
+    digitalWrite(relay3, HIGH);
+
+    //Transfer trigger
+    String index = String(id);
+    
+    //Transfer Sensor
+    String sensor = "|" ;
+    sensor += sensor0 ;
+    sensor += "|" ;
+    sensor += sensor1 ;
+    sensor += "|" ;
+    sensor += sensor2 ;
+    sensor +="|" ;
+    sensor+= sensor3;
+    
+    //Transfer Sensor Kelompok
+    String kelompok = "|" ;
+    kelompok += kelompok0 ;
+    kelompok += "|" ;
+    kelompok += kelompok1;
+    
+    //Transfer Waktu
+    String waktu = "|" ;
+    waktu += jam ;
+    waktu += "|" ;
+    waktu += menit; 
+    waktu += "|" ;
+    waktu += detik;
+
+    //Transfer tanggal
+    String tanggal = "|" ;
+    tanggal += hari ;
+    tanggal += "|" ;
+    tanggal += bulan ;
+    tanggal += "|" ;
+    tanggal += tahun;
+    tanggal += "|" ;
+
+    //Keseluruhan
+    pesan = index + sensor + kelompok + waktu + tanggal;
+    Serial.println(pesan);
+
+    for(int i = 0; i< pesan.length(); i++){
+      s.write(pesan[i]);  
     }
+
+//    if(s.available()>0){
+//      s.write(pesan);
+//    }
+
+    sensor = "";
+    kelompok = "";
+    waktu = "";
+    tanggal = "";
+    index = "";
+    
+    delay(30000);
+
+    //Mematikankeran air
+    digitalWrite(relay2, LOW);
+    digitalWrite(relay3, LOW);
+
+    //Calibrating    
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(" Automisasi Tanaman");
+    lcd.setCursor(0,1);
+    lcd.print("       Cabai");
+    lcd.setCursor(0,2);
+    lcd.print("Memeriksa Kelembapan");
+    lcd.setCursor(0,3);
+    lcd.print("     Calibrating");
+
+    //Delay 30 menit untuk penyesuaian kelembapan
+    
+    delay(1800000);
+    
   }
 }
